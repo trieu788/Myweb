eb731c55e9226eaf04074695d087c46419f8ae5d
trieu788 <71578314+trieu788@users.noreply.github.com>
2021-11-10 22:55:50 +0700

    Update db.js

--- atn-cloudDB/src/config/db.js
+++ atn-cloudDB/src/config/db.js
@@ -1,9 +1,9 @@
 const db = require('pg').Pool
 module.exports = new db({
-    user: "nilgxolznqcrhh",
-    host: "ec2-54-158-247-97.compute-1.amazonaws.com",
-    database: "d42t0t9nrhpv61",
-    password: "80ba106ba0ebccf773a87899edc54cae5c8c699c082b329a69dff7627f1f2d29  ",
+    user: "gqrxyzrethkvib",
+    host: "ec2-3-227-149-67.compute-1.amazonaws.com",
+    database: "d5mjnea23avvuq",
+    password: "18cf89d7029b948c8787072892e64d891c5cdcadc83f05ed7eac208d45032bb7  ",
     port: "5432",
     ssl: {
         rejectUnauthorized: false,
