1c63d783f8dff467624922811f193ecd29c5823f
trieu788 <71578314+trieu788@users.noreply.github.com>
2021-11-10 23:06:28 +0700

    Update db.js

--- atn-cloudDB/src/config/db.js
+++ atn-cloudDB/src/config/db.js
@@ -3,7 +3,7 @@ module.exports = new db({
     user: "gqrxyzrethkvib",
     host: "ec2-3-227-149-67.compute-1.amazonaws.com",
     database: "d5mjnea23avvuq",
-    password: "18cf89d7029b948c8787072892e64d891c5cdcadc83f05ed7eac208d45032bb7  ",
+    password: " 18cf89d7029b948c8787072892e64d891c5cdcadc83f05ed7eac208d45032bb7  ",
     port: "5432",
     ssl: {
         rejectUnauthorized: false,
